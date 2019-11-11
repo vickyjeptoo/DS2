f507506bb2d9150f510c291df9359d947fa3e966
vickyjeptoo <42605269+vickyjeptoo@users.noreply.github.com>
2019-11-11 04:53:36 -0500

    Add files via upload

---
+++ DataScienceVickyJeptoo/mllesson1.py
@@ -0,0 +1,14 @@
+#ML is a process where computers learn from data using specific algorithims
+#Once the computer learns from data,they find trends in data, correlation & relationships between variables
+#Afterwards the ccomputer can predict,recommend something from the data,plastering,looking at data and getting similar things.
+#Types of learning:
+#1.Machine Learning and Deep Learning
+#ML:
+#Supervised and Unsupervised learning
+#Supervised-Computers are given inputs and outputs to learn from
+    #Classsification and Regression
+#Unsupervised-Machine is given inputs but not outputs
+    #Clustering
+
+#Deep learning-advanced of ML works more on unsupervised learning
+#Example;
