adc148e75383e8d43a4102dedd46c2ab664f5560
vickyjeptoo <42605269+vickyjeptoo@users.noreply.github.com>
2019-11-13 04:49:34 -0500

    Add files via upload

---
+++ DataScienceVickyJeptoo/tableau.py
@@ -0,0 +1,10 @@
+#tableau online
+#test3@modkenya.com
+#Modcom101
+
+#create a workbook
+#then file-save as
+
+#load data
+#menu-data-connect to new data source
+#data is saved into dimensions and measures
